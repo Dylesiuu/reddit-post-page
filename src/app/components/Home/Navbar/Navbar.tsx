9278e48312c7803931527387897fbe0833169bd4
NO_NAME <dylewskidom1@gmail.com>
2025-04-03 00:34:56 +0200

    Fix padding. Add search bar

--- src/app/components/Home/Navbar/Navbar.tsx
+++ src/app/components/Home/Navbar/Navbar.tsx
@@ -1,28 +1,47 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from 'react'
 import Image from 'next/image'
-import { BiBell } from 'react-icons/bi'
+import { BiBell, BiSearch } from 'react-icons/bi'
 
 const Navbar = () => {
-  return (
-    <div className='w-full h-16 bg-gray shadow-md p-4 border-b border-b-gray-800'>
-      <div className='flex items-center h-full justify-between w-full px-4'>
-        {/* Logo */}
-    <div className='flex items-center space-x2'>
-        <div className='w-10 h-10 rounded-full flex items-center justify-center'>
-            <Image src="/images/Reddit_Logo.png" alt="logo" width={100} height={100} className='rounded-full' />
-        </div>
-        </div>
-    {/* Search Bar */}
-    <div className='flex-1 flex items-center justify-center'>
-        <p className='text-white'>Search Reddit</p>
-    </div>
 
-    {/* User Profile */}
-    <div className='flex items-center space-x-4'>
-        <BiBell className='text-white' size={24} />
-        <p className='text-white'>Profile</p> 
+  const [searchText, setSearchText] = useState('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value);
+    console.log(e.target.value);
+  };
+  return (
+    <div className='w-full h-13 bg-gray shadow-md p-3 border-b border-b-gray-800'>
+      <div className='flex items-center h-full justify-between w-full'>
+        {/* Logo */}
+        <div className='flex items-center space-x2'>
+            <div className='w-8 h-8 sm:block rounded-full flex items-center justify-center'>
+                <Image src="/images/Reddit_Logo.png" alt="logo" width={100} height={100} className='rounded-full' />
+            </div>
+            <h1 className='text-3xl sm:text-orange-600 md:text-white font-bold pl-1.5'>reddit</h1>
         </div>
-    </div>
+        {/* Search Bar */}
+        <div className='flex-1 flex items-center justify-center p-1'>
+          <div className='relative w-full max-w-md'>
+            <BiSearch className='absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-300' size={19} />
+            <input 
+              type="text" 
+              value={searchText}
+              onChange={handleInputChange}
+              placeholder="Search Reddit" 
+              className='w-full pl-9 py-2 rounded-full bg-gray-700 text-white text-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-white'/>
+          </div>
+          
+        </div>
+
+        {/* User Profile */}
+        <div className='flex items-center space-x-3'>
+            <BiBell className='text-white' size={24} />
+            <p className='text-white'>Profile</p> 
+        </div>
+      </div>
     </div>
   )
 }
