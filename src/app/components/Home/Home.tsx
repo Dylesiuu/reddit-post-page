854428fcd76b7e2acae96ed5fbde61f55beeac3a
NO_NAME <dylewskidom1@gmail.com>
2025-04-03 22:43:35 +0200

    Improve top navbar. Add moveable navbar

--- src/app/components/Home/Home.tsx
+++ src/app/components/Home/Home.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Home = () => {
-    return <div className="overflow-hidden">Home</div>
+    return(
+    <div className="overflow-hidden justify-center">
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold text-white">Welcome to Reddit</h1>
+            <p className="mt-4 text-lg text-gray-400">Explore the best posts and discussions on Reddit, the front page of the internet.</p>
+        </div>
+    </div>
+    );
 };
 
 export default Home;
--- src/app/components/Navbar/MobileNavbar.tsx
+++ src/app/components/Navbar/MobileNavbar.tsx
@@ -1,11 +1,41 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from 'react';
 
 const MobileNavbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default MobileNavbar
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="relative ">
+      {/* Navbar */}
+      <div
+        className={`fixed top-15 left-0 h-full w-64 bg-gray border-r border-r-gray-800 text-white transform transition-transform duration-500 z-[1002] ${
+          isOpen ? 'translate-x-0' : '-translate-x-full'
+        }`}
+      >
+        <h2 className="p-4 text-lg font-bold">Mobile Navbar</h2>
+        <ul className="p-4 space-y-2">
+          <li className="hover:bg-gray-700 p-2 rounded">Home</li>
+          <li className="hover:bg-gray-700 p-2 rounded">About</li>
+          <li className="hover:bg-gray-700 p-2 rounded">Contact</li>
+        </ul>
+      </div>
+
+      {/* Toggle Button */}
+      <button
+        className={`fixed top-[calc(15 + 1rem)] left-4 z-[1003] bg-gray-700 text-white p-2 rounded transform transition-transform duration-500 ${
+          isOpen ? 'translate-x-64' : ''
+        }`}
+        onClick={toggleNavbar}
+      >
+        Menu
+      </button>
+    </div>
+  );
+};
+
+export default MobileNavbar;
