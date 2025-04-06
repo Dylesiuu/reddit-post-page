d16b5853cff8b7e0716823955ed7c86efd472504
NO_NAME <dylewskidom1@gmail.com>
2025-04-06 14:07:16 +0200

    Fix post page display

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 const PostPage = async ({params} : {params: {id:string}} ) => {
-  const id = (await params).id;
+  const id = params.id;
   return (
-    <div className="text-5x1 text-orange-700 font-bold">
-      Post with id: {id}
-    </div>
+     <div className="overflow-hidden justify-center">
+     <div className="flex flex-col items-center justify-center h-screen">
+         <h1 className="text-4xl text-orange-700 font-bold">Post with id: {id}</h1>
+     </div>
+ </div>
   );
 }
 export default PostPage;
